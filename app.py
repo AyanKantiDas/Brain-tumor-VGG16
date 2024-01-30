e365b3a5f1aad02b2e00976870de1938acffe3e3
Ayan Kanti Das <103057066+AyanKantiDas@users.noreply.github.com>
2024-01-30 05:45:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+import os
+# %%
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Dropout
+from sklearn.metrics import accuracy_score
+# %%
+import ipywidgets as widgets
+import io
+from PIL import Image
+import tqdm
+from sklearn.model_selection import train_test_split
+import cv2
+from sklearn.utils import shuffle
+import tensorflow as tf
+
+# %%
+X_train = []
+Y_train = []
+image_size = 150
+labels = ['glioma', 'meningioma', 'notumor', 'pituitary']
+for i in labels:
+    folderPath = os.path.join('C:/Users/Ayan Kanti Das/OneDrive/Desktop/brain/archive/Training', i)
+    for j in os.listdir(folderPath):
+        img = cv2.imread(os.path.join(folderPath, j))
+        img = cv2.resize(img, (image_size, image_size))
+        X_train.append(img)
+        Y_train.append(i)
+
+for i in labels:
+    folderPath = os.path.join('C:/Users/Ayan Kanti Das/OneDrive/Desktop/brain/archive/Testing', i)
+    for j in os.listdir(folderPath):
+        img = cv2.imread(os.path.join(folderPath, j))
+        img = cv2.resize(img, (image_size, image_size))
+        X_train.append(img)
+        Y_train.append(i)
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+# %%
+X_train, Y_train = shuffle(X_train, Y_train, random_state=101)
+# %%
+X_train, X_test, y_train, y_test = train_test_split(X_train, Y_train, test_size=0.1, random_state=101)
+# %%
+y_train_new = []
+for i in y_train:
+    y_train_new.append(labels.index(i))
+
+y_train = y_train_new
+y_train = tf.keras.utils.to_categorical(y_train)
+
+# now do the same for test
+
+y_test_new = []
+for i in y_test:
+    y_test_new.append(labels.index(i))
+y_test = y_test_new
+y_test = tf.keras.utils.to_categorical(y_test)
+# %%
+model = Sequential()
+# this model is inspired by VGG 16
+# layer1
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.3))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Dropout(0.3))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.3))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.3))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(4, activation='softmax'))
+# %%
+model.summary()
+# %%
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+# %%
+history = model.fit(X_train, y_train, epochs=20, validation_split=0.1)
+# %%
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# %%
+model.save('braintumorvgg16.h5')
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+epochs = range(len(acc))
+fig = plt.figure(figsize=(14, 7))
+plt.plot(epochs, acc, 'r', label="Train Accuracy")
+plt.plot(epochs, val_acc, 'b', label="Validation Accuracy")
+plt.legend(loc='upper left')
+plt.show()
+# %%
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(loss))
+fig = plt.figure(figsize=(14, 7))
+plt.plot(epochs, loss, 'r', label="Train Loss")
+plt.plot(epochs, val_loss, 'b', label="Validation Loss")
+plt.legend(loc='upper left')
+plt.show()
+# %%
+img = cv2.imread('5 no.jpg')
+
+img = cv2.resize(img, (150, 150))
+
+img_array = np.array(img)
+
+img_array.shape
+
+img_array = img_array.reshape(1, 150, 150, 3)
+img_array.shape
+
+from tensorflow.keras.preprocessing import image
+
+img = image.load_img('5 no.jpg')
+plt.imshow(img, interpolation='nearest')
+
+ans = model.predict(img_array)
+indices = ans.argmax()
+
+if indices == 0:
+    print("Glioma Tumor Found")
+elif indices == 1:
+    print("Meningioma Tumor Found")
+elif indices == 2:
+    print("No Tumor Found")
+elif indices == 3:
+    print("Pituitary Tumor Found")
+# %%
